1925abda41c252df91e5048356bd97ed7ceba2db
MDeMel-Dev <mdemel.dev@gmail.com>
2026-08-31 13:46:10 +1000

    Implement a viewmodel for first fragment screen

--- app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/FirstScreenFragment.kt
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/FirstScreenFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.vu.nit3213sandboxapp.R
 
 class FirstScreenFragment: Fragment() {
 
+    private lateinit var firstScreenFragmentViewModel : FirstScreenFragmentViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
     Bundle?): View? {
+        firstScreenFragmentViewModel = FirstScreenFragmentViewModel()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first_screen, container, false)
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,5 +25,6 @@ class FirstScreenFragment: Fragment() {
         // Initialize views and set up fragment logic here
         // val button = view.findViewById<Button>(R.id.button)
 
+        view.findViewById<TextView>(R.id.firstScreenFragmentTitle).text = firstScreenFragmentViewModel.screenTitle
     }
 }
---
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/FirstScreenFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package com.vu.nit3213sandboxapp.ui.sessionfour
+
+import androidx.lifecycle.ViewModel
+
+class FirstScreenFragmentViewModel: ViewModel() {
+
+    val screenTitle = "First Fragment Screen"
+}
