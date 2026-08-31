1aa9ad8ff2186ad1ad2acec58bb5229f21057b7d
MDeMel-Dev <mdemel.dev@gmail.com>
2026-08-31 14:29:56 +1000

    Create new fragments for new screens

--- app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/FirstScreenFragment.kt
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/FirstScreenFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.vu.nit3213sandboxapp.R
 
 class FirstScreenFragment: Fragment() {
 
     private lateinit var firstScreenFragmentViewModel : FirstScreenFragmentViewModel
+    private lateinit var screenBNavigationButton: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
     Bundle?): View? {
@@ -25,6 +28,15 @@ class FirstScreenFragment: Fragment() {
         // Initialize views and set up fragment logic here
         // val button = view.findViewById<Button>(R.id.button)
 
+        screenBNavigationButton = view.findViewById<Button>(R.id.screenBNavigationButton)
+
         view.findViewById<TextView>(R.id.firstScreenFragmentTitle).text = firstScreenFragmentViewModel.screenTitle
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        screenBNavigationButton.setOnClickListener {
+            findNavController().navigate(R.id.action_firstScreenFragment_to_screenBFragment)
+        }
     }
 }
---
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/ScreenCFragment.kt
@@ -0,0 +1,23 @@
+package com.vu.nit3213sandboxapp.ui.sessionfour
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vu.nit3213sandboxapp.R
+
+class ScreenCFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_screen_c, container, false)
+    }
+}
