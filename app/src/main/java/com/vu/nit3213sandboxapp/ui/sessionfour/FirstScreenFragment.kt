aeb270c0374547de692e0f6c4d9c04ceaad7ac81
MDeMel-Dev <mdemel.dev@gmail.com>
2026-08-31 13:34:47 +1000

    Create a simple fragment

---
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionfour/FirstScreenFragment.kt
@@ -0,0 +1,25 @@
+package com.vu.nit3213sandboxapp.ui.sessionfour
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vu.nit3213sandboxapp.R
+
+class FirstScreenFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
+    Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initialize views and set up fragment logic here
+        // val button = view.findViewById<Button>(R.id.button)
+
+    }
+}
