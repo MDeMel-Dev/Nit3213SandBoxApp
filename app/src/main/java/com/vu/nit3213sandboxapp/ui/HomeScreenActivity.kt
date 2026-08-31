196c57a2ef82bde643246525278463c6400a50c6
MDeMel-Dev <mdemel.dev@gmail.com>
2026-08-31 13:34:21 +1000

    Create a fragment host activity

--- app/src/main/java/com/vu/nit3213sandboxapp/ui/HomeScreenActivity.kt
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/HomeScreenActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.vu.nit3213sandboxapp.R
+import com.vu.nit3213sandboxapp.ui.sessionfour.FragmentHostActivity
 import com.vu.nit3213sandboxapp.ui.sessionone.FirstScreenActivity
 import com.vu.nit3213sandboxapp.ui.sessiontwo.ConstraintLayoutExerciseActivity
 
@@ -15,6 +16,7 @@ class HomeScreenActivity : AppCompatActivity() {
 
     private lateinit var sessionOneNavigationButton: Button
     private lateinit var sessionTwoNavigationButton: Button
+    private lateinit var sessionThreeNavigationButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +35,7 @@ class HomeScreenActivity : AppCompatActivity() {
     fun getViewReferences() {
         sessionOneNavigationButton = findViewById(R.id.s1NavigationButton)
         sessionTwoNavigationButton = findViewById(R.id.s2NavigationButton)
+        sessionThreeNavigationButton = findViewById(R.id.s3FragmentsNavigationButton)
     }
 
     fun setListeners() {
@@ -42,6 +45,10 @@ class HomeScreenActivity : AppCompatActivity() {
 
         sessionTwoNavigationButton.setOnClickListener {
             startActivity((Intent(this, ConstraintLayoutExerciseActivity::class.java)))
+        }
+
+        sessionThreeNavigationButton.setOnClickListener {
+            startActivity((Intent(this, FragmentHostActivity::class.java)))
         }
     }
 }
