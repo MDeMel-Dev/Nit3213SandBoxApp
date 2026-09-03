acec249bfbcee4f1fcd11e37ef76ba50f81f3ce6
MDeMel-Dev <mdemel.dev@gmail.com>
2026-09-03 14:09:25 +1000

    Create viewholder

---
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionsix/PhoneListItemViewHolder.kt
@@ -0,0 +1,15 @@
+package com.vu.nit3213sandboxapp.ui.sessionsix
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vu.nit3213sandboxapp.R
+import com.vu.nit3213sandboxapp.network.data.Phone
+
+class PhoneListItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val textView: TextView = view.findViewById(R.id.phoneItemText)
+
+    fun bind(item: Phone) {
+        textView.text = item.name
+    }
+}
