68912f4b5d026a2c2fd755902fc3a3d18e207525
MDeMel-Dev <mdemel.dev@gmail.com>
2026-09-03 14:09:49 +1000

    Create Adapter

---
+++ app/src/main/java/com/vu/nit3213sandboxapp/ui/sessionsix/PhoneListAdapter.kt
@@ -0,0 +1,31 @@
+package com.vu.nit3213sandboxapp.ui.sessionsix
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.vu.nit3213sandboxapp.R
+import com.vu.nit3213sandboxapp.network.data.Phone
+
+class PhoneListAdapter(private val dataList: MutableList<Phone> = mutableListOf<Phone>()) : RecyclerView.Adapter<PhoneListItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhoneListItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+                R.layout.phone_item_layout, parent,
+                false
+            )
+
+        return PhoneListItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PhoneListItemViewHolder, position: Int) {
+        holder.bind(dataList[position])
+    }
+
+    override fun getItemCount() = dataList.size
+
+    fun setData(phoneListData: List<Phone>) {
+        dataList.clear()
+        dataList.addAll(phoneListData)
+        notifyDataSetChanged()
+    }
+}
